25019939ab9cfd8fc569b826e83566ad765fb0b8
f0r4e <137251152+f0r4e@users.noreply.github.com>
2024-10-30 12:05:09 +0800

    Update Al.java

--- src/main/java/com/aluminum/Al.java
+++ src/main/java/com/aluminum/Al.java
@@ -9,15 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Al implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("al");
 	public static final GameRules.Key<GameRules.IntRule> PISTON_PUSHING_LIMIT =  GameRuleRegistry.register("PistonPushingLimit", GameRules.Category.MISC, GameRuleFactory.createIntRule(12,0));
 
 	public static final GameRules.Key<GameRules.BooleanRule> PISTON_FORCE_PUSH = GameRuleRegistry.register("PistonForcePush",GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 	@Override
 	public void onInitialize() {
-		LOGGER.info("aluminum has been loaded!");
+		LOGGER.info("Aluminum has been loaded!");
 
 	}}
